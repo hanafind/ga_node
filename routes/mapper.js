71e46ad4407f57d6ed2dada043de794575b95f33
leejaejin <l2jjang@gmail.com>
2023-01-03 17:24:46 +0900

    audit

--- routes/mapper.js
+++ routes/mapper.js
@@ -58,8 +58,9 @@ let sqlPostView =
 `SELECT
     p.idx,
     pc.idx as category_idx, pc.name, pc.name_ko,
-    p.title, p.contents, p.created_at, p.posting_date, p.is_audit, p.audit_grant_start_date, p.audit_grant_end_date, p.is_visible,
-    p.cover_image_url, p.url_slug, p.cover_type, p.cover_video_url, p.meta_title, p.meta_desc, p.meta_keywords,` 
+    p.title, p.contents, p.created_at, p.posting_date, p.is_audit, p.audit_num_index, 
+    (p.audit_grant_start_date || '~' || p.audit_grant_end_date) as audit_date, 
+    p.is_visible, p.cover_image_url, p.url_slug, p.cover_type, p.cover_video_url, p.meta_title, p.meta_desc, p.meta_keywords,` 
    + sqlInterval 
    + ` FROM
         blog.posts as p, blog.posts_post_categories_map as pcm, blog.post_categories as pc

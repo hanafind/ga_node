16dad2a70ae90b2116361bbcc8a1812f056b7ab6
leejaejin <l2jjang@gmail.com>
2023-02-08 15:47:53 +0900

    faq

--- routes/faq.js
+++ routes/faq.js
@@ -2,16 +2,51 @@ var express = require("express");
 var router = express.Router();
 var modules = require("../modules");
 var mapper = require("./mapper.js");
+// const axios = require('axios');
 
-/* GET article page. */
+/* GET fag */
 router.get("/", async function (req, res) {
-    try {
-      res.render("faq", {
-      });
-    } catch (err) {
-      res.redirect("/");
+  try {
+    // 포스트 목록
+    let sql = `select * from blog.faq;`;
+    let result = await modules.pg.query(sql);
+    let data = {insurance:[], finance:[], life:[]};
+
+    for (let i=0; i<result.length; i++) {
+        if (result[i].category === 'insurance') {
+            data.insurance.push(result[i]);
+        } else if (result[i].category === 'finance') {
+            data.finance.push(result[i]);
+        } else {
+            data.life.push(result[i]);
+        }
     }
-  });
+    res.render("faq", {
+        title: '자주하는 질문',
+        data: data,
+    });
+  } catch (err) {
+    res.redirect("/");
+  }
+});
 
+router.post("/consult", async function (req, res) {
+    try {
+        let sql = `SELECT last_value as seq FROM cs.consult_history_idx_seq;`;
+        let result = await modules.pg.query(sql);
+        console.log('result : ' + result[0].seq);
 
-  module.exports = router;
+        req.body.seq = result[0].seq + 1;
+        req.body.inflow_route = 'HFF_IN_01';
+        
+        // let result_consult = await modules.axios.axiosPost(req.body);
+
+        res.status(200).json({msg: "suc"});
+        // console.log('result : ' + JSON.stringify(result));
+
+    } catch (err) {
+
+    }
+});
+
+module.exports = router;

24138e45066c68cc560145d785f9f39fd1765502
hanafind <hanadevhana@gmail.com>
2023-02-21 13:54:20 +0900

    logo 수정

--- routes/product.js
+++ routes/product.js
@@ -10,7 +10,8 @@ router.get("/", async function (req, res) {
     res.data = {};
     res.data.products_recommend = await modules.pg.query(mapper.sqlDirectRecommend);
     res.data.products_all = await modules.pg.query(mapper.sqlDirectAll);
-
+    res.data.partners = await modules.pg.query(`SELECT name, logo_url FROM insurance.company`);
+    console.log(res.data.partners);
     res.render("product/", {req: req, res: res});
   } catch (err) {
     console.log(err);
